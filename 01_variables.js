57800f93a6d7be1dd3903a3e02c83dc536011ce0
biren1112 <birenpatel0210@gmail.com>
2024-06-09 15:18:02 +0530

    variables

---
+++ 01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "biren@google.com"
+var accountPassword = "1234"
+accountCity = "Patan"
+let accountState;
+
+//accountId = 2;
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
