09873c159a0728e39ca78d5afbee1b918a222d95
biren1112 <birenpatel0210@gmail.com>
2024-06-09 16:17:43 +0530

    conversion of datatypes

---
+++ 03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "biren";
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//In Number conversion
+// "28" => 28
+// "28abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "biren"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+//In Boolean conversion 
+// 1 => true; 0 => false
+// "" => false
+// "biren" => true
+
+let someNumber = 28;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
