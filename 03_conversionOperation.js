49aae878bf04c1bd876e23c20c3fdf91196e108d
biren1112 <birenpatel0210@gmail.com>
2024-06-09 17:01:29 +0530

    updated code in conversion

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -27,5 +27,25 @@ let someNumber = 28;
 
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************** Operations ************************
+
+let value = 28
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello"
+let str2  = " biren"
+
+let str3 = str1+str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
