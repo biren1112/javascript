7e650b5e1c16c90e7c249b386337616c9b1194cd
biren1112 <birenpatel0210@gmail.com>
2024-06-09 15:59:47 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer code
+
+//alert(3+3); // we are using node js not browser
+
+console.log(3
+    + 3
+); // code readability should be high
+
+console.log("biren");
+
+let name = "biren";
+let age = 18;
+let isLoggedIn = true;
+let state;
+
+let a = 2;
+// {
+// let a = 4
+// }
+console.log(a);
+
+
+console.log(typeof state); // undefined
+console.log(typeof undefined); //undefined
+
+console.log(typeof null); //object
